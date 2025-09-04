3e0ac8efc56f27202f5a659868fa76e1f823d704
Will Ford <william.ford0@gmail.com>
2025-09-04 09:20:05 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,22 @@
+export default {
+    supabase: {
+        url: 'https://xgzyguuusjfyqpztzipb.supabase.co',
+        anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhnenlndXV1c2pmeXFwenR6aXBiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY2NDQ2MzYsImV4cCI6MjA3MjIyMDYzNn0.0Ck_lfzwsKVt7OWutETZSnPFcjDCXXAjhGIKD-cps7s'
+    },
+    apis: {
+        polygon: {
+            apiKey: 'wfpt139nvZFbUkwpoOXI1Z5Zv98aIwhQ'
+        },
+        vantage: {
+            apiKey: '5AOC2TFFFH6BQF1M'
+        }
+    },
+    symbols: {
+        primary: 'SPY',
+        secondary: ['IWM', 'QQQ']
+    },
+    trading: {
+        defaultPositionSize: 1,
+        maxPositions: 3
+    }
+}
